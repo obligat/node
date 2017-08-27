a6460e11b2154c1112999ab4419bd359eb600ec6
dujinqiao <duzi1996@qq.com>
2017-08-27 19:00:12 +0800

    mongo

--- mongo/mongodb.js
+++ mongo/mongodb.js
@@ -1,46 +1,54 @@
 'use strict';
 const mongoose = require('mongoose');
-const db = mongoose.connect('mongodb://localhost:27017/minion');
+mongoose.connect('mongodb://localhost:27017/minion');
 
-db.connection.on('error', function (err) {
+const db = mongoose.connection;
+
+db.on('error', function (err) {
     console.log('database connect fail' + err);
 });
 
-db.connection.on('open', () => {
+db.once('open', () => {
     console.log('connect !');
 });
 
-/*
-const MyShema = new mongoose.Schema({
+const mySchema =mongoose.Schema({
     name: {type: String},
     age: {type: Number, default: 0},
     time: {type: Date, default: Date.now}
 });
 
-const MyModel = db.model("Minion", MyShema);
+mySchema.methods.speak = function () {
+
+    console.log('I am ' + this.name);
+};
+
+const MyModel = mongoose.model("Minion", mySchema);
+
 
 let entity = new MyModel({
     name: 'pep',
     age: 1
 });
 
-console.log(entity.name);
-console.log(entity.time);
 
-entity.save((err, doc) => {
+entity.speak();
+
+
+/*entity.save((err, doc) => {
     if (err) {
         console.log(err);
     } else {
         console.log(doc);  // save into doc
     }
-});
+});*/
 
-MyModel.find({}, (err, docs) => {
+/*MyModel.find({}, (err, docs) => {
     console.log(docs);
 
-});
+});*/
 
-MyModel.create({name: 'da', age: 9},(err,doc)=>{
+/*MyModel.create({name: 'da', age: 9},(err,doc)=>{
     if(err){
         console.log(err);
     }else{
